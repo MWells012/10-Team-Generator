6e3fb349991f78d323e56187a6a8b91af5dba20c
Michaela Wells <mgwells012@gmail.com>
2022-12-15 17:22:18 -0500

    added files for tests and for all the info required on the cards. attempted to write the info for the html generator as well

---
+++ dist/card-generator.js
@@ -0,0 +1,70 @@
+const addManager = (data) => {
+    return 
+    `
+    
+    <div class="col-4-sm-12 ">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-header text-center"> 
+                        <h5> Name </h5>
+                        <h6> Manager </h6> 
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"> ID Number: </li>
+                            <li class="list-group-item"> Email: <a href="email@mail.com"> email@mail.com</a> </li>
+                            <li class="list-group-item"> Office Number: </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    
+    `
+}
+
+const addEngineer = (data) => {
+    return
+    `
+    <div class="col-4-sm-12 ">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-header text-center"> 
+                        <h5> Name </h5>
+                        <h6> Engineer </h6> 
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"> ID Number: </li>
+                            <li class="list-group-item"> Email: <a href="email@mail.com"> email@mail.com</a> </li>
+                            <li class="list-group-item"> Office Number: </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    
+    `
+}
+
+const addIntern = (data) => {
+    return
+    `
+    
+    <div class="col-4-sm-12 ">
+                <div class="card" style="width: 20rem;">
+                    <div class="card-header text-center"> 
+                        <h5> Name </h5>
+                        <h6> Intern </h6> 
+                    </div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"> ID Number: </li>
+                            <li class="list-group-item"> Email: <a href="email@mail.com"> email@mail.com</a> </li>
+                            <li class="list-group-item"> Office Number: </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+        `
+}
