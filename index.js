0304b6594225f59575241f4e7db714ac3610a654
Michaela Wells <mgwells012@gmail.com>
2023-01-04 13:07:25 -0500

    finished adding the functions to index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const inquirer = require ('inquirer');
 const fs = require ('fs');
-
-const manager = require ('./lib/manager');
-const engineer = require ('./lib/engineer');
-const intern = require ('./lib/intern');
-const employee = require ('./lib/employee');
-
+const Employee = require ('./lib/employee');
+const Manager = require ('./lib/manager');
+const Engineer = require ('./lib/engineer');
+const Intern = require ('./lib/intern');
 const HTMLteamPage = require ('./dist/card-generator');
+const { off } = require('process');
 
 const theTeam = [];
 
 function addManager() {
-    console.log("Welcome! Here you can build your team. To create a card for an individual please follow the following prompts:")
-    inquirer.createPromptModule([
+    console.log("Welcome! This application allows you to build your team and view it on one platform. To create a card for an individual please follow the following prompts:")
+    inquirer.prompt([
         {
             type:"input",
             message: "Name?",
@@ -28,10 +27,10 @@ function addManager() {
         },
         {
             type:"input",
-            message: "Name?",
-            name: "managerName",
-            validate: managerName => {
-                if(managerName){
+            message: "Employee ID number?",
+            name: "managerID",
+            validate: managerID => {
+                if(managerID){
                     return true;
                 } else {
                     console.log("This is mandatory, please try again.");
@@ -41,10 +40,10 @@ function addManager() {
         },
         {
             type:"input",
-            message: "Name?",
-            name: "managerName",
-            validate: managerName => {
-                if(managerName){
+            message: "Email?",
+            name: "managerEmail",
+            validate: managerEmail => {
+                if(managerEmail){
                     return true;
                 } else {
                     console.log("This is mandatory, please try again.");
@@ -54,10 +53,10 @@ function addManager() {
         },
         {
             type:"input",
-            message: "Name?",
-            name: "managerName",
-            validate: managerName => {
-                if(managerName){
+            message: "Office Number?",
+            name: "officeNumber",
+            validate: officeNumber => {
+                if(officeNumber){
                     return true;
                 } else {
                     console.log("This is mandatory, please try again.");
@@ -94,12 +93,101 @@ function addEmployee() {
 }
 
 function addEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Name?",
+            name: "engineerName",
+            validate: engineerName => {
+                if(engineerName){
+                    return true;
+                } else {
+                    console.log("This is mandatory, please try again.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "Employee ID Number?",
+            name: "engineerID",
+            validate: engineerID => {
+                if(engineerID){
+                    return true;
+                } else {
+                    console.log("This is mandatory, please try again.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "Email?",
+            name: "engineerEmail",
+            validate: engineerEmail => {
+                if(engineerEmail){
+                    return true;
+                } else {
+                    console.log("This is mandatory, please try again.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            message: "Github Username?",
+            name: "engineerGithub",
+            validate: engineerGithub => {
+                if(engineerGithub){
+                    return true;
+                } else {
+                    console.log("This is mandatory, please try again.");
+                    return false;
+                }
+            }
+        },
+    ]).then (data => {
+        const engineer = new Engineer(data.engineerName, data.engineerEmail, data.engineerGithub, data.engineerID);
+        theTeam.push(engineer);
+        menu();
+    })
 
 }
+
 function addIntern() {
 
 }
 
 function menu() {
-    return inqui
+    return inquirer.createPromptModule([
+        {
+            type:"list",
+            message: "Add another employee?",
+            name:"addEmployee",
+            choices: ["Yes", "Nope"]
+        }
+    ]).then (function(answers) {
+        switch(answers.addEmployee) {
+            case "Yes":
+            addEmployee();
+            break;
+            case "Nope":
+                createTheTeam();
+                break;
+        }
+    });
+};
+
+function createTheTeam() {
+    const renderHTML = HTMLteamPage(theTeam)
+    fs.writeFile('./index.html', renderHTML, err => {
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            console.log ("Your team has been successfully created and written into an index.html file.")
+        }
+    })
 }
+
+addManager();
